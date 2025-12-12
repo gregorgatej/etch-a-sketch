59343e6f2f1740a0dcb7c6e964a8b0c2882e0f43
Gregor Gatej <gregor.gatej@gmail.com>
2025-12-12 16:49:48 +0100

    Add hover effect to grid divs

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
+const containerDiv = document.querySelector(".container");
 for (let i = 0; i < 16; i++) {
     const rowDiv = document.createElement("div");
     rowDiv.classList.add("row");
-    const containerDiv = document.querySelector(".container");
     containerDiv.appendChild(rowDiv);
-
         for (let j = 0; j < 16; j++) {
             const columnDiv = document.createElement("div");
             columnDiv.classList.add("column")
-            columnDiv.innerText = "Column Div";
             rowDiv.appendChild(columnDiv);
         }
 }
+
+containerDiv.addEventListener('mouseover', e => {
+    hoveredOverDiv = e.target;
+    hoveredOverDiv.classList.add("pink");
+});
