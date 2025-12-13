2ed5a14246379ec56e8f61afa2c1639635df64d6
Gregor Gatej <gregor.gatej@gmail.com>
2025-12-13 10:57:18 +0100

    Add option to change nr. of squares per side

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const containerDiv = document.querySelector(".inner-container");
+const innerContainerDiv = document.querySelector(".inner-container");
 for (let i = 0; i < 16; i++) {
     const rowDiv = document.createElement("div");
     rowDiv.classList.add("row");
-    containerDiv.appendChild(rowDiv);
+    innerContainerDiv.appendChild(rowDiv);
         for (let j = 0; j < 16; j++) {
             const columnDiv = document.createElement("div");
             columnDiv.classList.add("column")
@@ -10,7 +10,37 @@ for (let i = 0; i < 16; i++) {
         }
 }
 
-containerDiv.addEventListener('mouseover', e => {
+innerContainerDiv.addEventListener("mouseover", e => {
     if (!e.target.classList.contains("column")) return;
     e.target.classList.add("pink");
+});
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", e => {
+    nrOfSquares = Number(prompt("Enter nr. of squares per side:"));
+    console.log(nrOfSquares);
+    while (Number.isNaN(nrOfSquares)) {
+        alert("Try again with inputting a number!");
+        nrOfSquares = Number(prompt("Enter nr. of squares per side:"));
+    }
+    while (nrOfSquares > 100) {
+        alert("Try again with inputting a smaller number!");
+        nrOfSquares = Number(prompt("Enter nr. of squares per side:"));
+    }
+    innerContainerDiv.innerHTML="";
+    for (let i = 0; i < nrOfSquares; i++) {
+    const rowDiv = document.createElement("div");
+    rowDiv.classList.add("row");
+    innerContainerDiv.appendChild(rowDiv);
+        for (let j = 0; j < nrOfSquares; j++) {
+            const columnDiv = document.createElement("div");
+            columnDiv.classList.add("column");
+            rowDiv.appendChild(columnDiv);
+            if (nrOfSquares > 50) {
+            columnDiv.setAttribute("style", "border: grey solid 1px");
+            innerContainerDiv.setAttribute("style", "border: grey solid 1px");
+            }
+        }
+}
 });
